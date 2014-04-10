19c3d0b5b6cb21faa58bbb7a10b1448ccd03cbc1
Sanghati Mukherjee <sanghatibm88@gmail.com>
2014-04-10 16:09:46 +0530

    Create phoney_keypad.rb

---
+++ phoney_keypad.rb
@@ -0,0 +1,17 @@
+require_relative './permutation'
+
+class PhoneKeypad
+	phone_number = []
+	validate_phone_number = []
+	puts "Enter the phone number : "
+	validate_phone_number = gets.to_i
+	if validate_phone_number = ~ /\d{3} - \d{4}/
+		phone_number = validate_phone_number.to_s.split('-',2)
+		permute_number = phone_number[1].chomp.split('')
+		permute = Permutation.new(permute_number)
+		permute.conversion_to_alphabets	
+	else
+	puts "Please Enter 8 digit only"
+	end
+	
+end
